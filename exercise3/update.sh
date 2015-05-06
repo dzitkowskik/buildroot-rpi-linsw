c3b5ac942bab83cd860e7df2fc7c76b6feadbbe7
Karol Dzitkowski <k.dzitkowski@gmail.com>
2015-05-06 22:52:26 +0200

    exercise 3 progress

---
+++ exercise3/update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir /mnt/rootfs
+mount /dev/mmcblk0p2 /mnt/rootfs
+cd /mnt/rootfs
+rm -rf *
+cd /mnt/sdcard
+umount /dev/mmcblk0p2
+cat rootfs.ext4 > /dev/mmcblk0p2
+mount /dev/mmcblk0p2 /mnt/rootfs
